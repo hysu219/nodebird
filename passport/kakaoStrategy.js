f645ff6143db5ca2c5870a7e9669296056e15612
hysu219 <wentto0@naver.com>
2021-06-11 09:42:40 +0900

    Initial commit

---
+++ passport/kakaoStrategy.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const KakaoStrategy = require('passport-kakao').Strategy;
+
+const User = require('../models/user');
+
+module.exports = () => {
+    passport.use(new KakaoStrategy ({
+        clientID: process.env.KAKAO_ID,
+        callbackURL: '/auth/kakao/callback',
+    }, async (accessToken, refreshToken, profile, done) => {
+        console.log('kako profile', profile);
+        try {
+            const exUser = await User.findOne({
+                where: { snsId: profile.id, provider: 'kakao' },
+            });
+            if (exUser) {
+                done(null, exUser);
+            } else {
+                const newUser = await User.create({
+                    email: profile.__json && profile.__json.kakao_account_email,
+                    nick: profile.displayName,
+                    snsId: profile.id,
+                    provider: 'kakao',
+                });
+                done(null, newUser);
+            }
+        } catch (error) {
+            console.error(error);
+            done(error);
+        }
+    }));
+};
